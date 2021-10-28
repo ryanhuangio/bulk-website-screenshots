3989617f4c29f2cd4654a2ea92f092b50df4c782
Ryan Huang <11435382+ryanhuangio@users.noreply.github.com>
2021-10-27 22:47:22 -0700

    Update screenshot-shell-generator.php
    Added timeout feature so it does not stall if the URL is bad. This requires coreutils to be installed.

--- screenshot-shell-generator.php
+++ screenshot-shell-generator.php
@@ -1,17 +1,19 @@
 <?php
 
-$domains = "google.com
-prdistribution.com
-mediaoutlets.com
-mediacontactslist.com";
+$URLs = "https://www.google.com
+https://www.prdistribution.com
+https://www.mediaoutlets.com
+https://www.mediacontactslist.com";
 
 $screenshotWidth = "1920";
 $screenshotHeight = "1200";
-$resize = "1400"; 
-// To turn off resizing, comment out line below that says: echo "sips -Z $resize $filename\n";
+$resize = "1400"; // To turn off resizing, comment out line below that says: echo "sips -Z $resize $filename\n";
+$timeout = "10"; // The script will wait 10 seconds on each URL 
 
-foreach(preg_split("/((\r?\n)|(\r\n?))/", $domains) as $domain){
+foreach(preg_split("/((\r?\n)|(\r\n?))/", $URLs) as $URL){
     $filename = str_replace(".","_",$domain).".png";
-    echo "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --ignore-certificate-errors --window-size=$screenshotWidth,$screenshotHeight https://$domain --screenshot=$filename\n";
+    $filename = str_replace("://","_",$filename);
+    $filename = str_replace("/","_",$filename);
+    echo "timeout $timeout /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --ignore-certificate-errors --window-size=$screenshotWidth,$screenshotHeight $URL --screenshot=$filename\n";
     echo "sips -Z $resize $filename\n";
 } 
